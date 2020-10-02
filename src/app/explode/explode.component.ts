d943185601be7a37a1e7a57904a91590b003576d
sanderelias <sanderelias+git@gmail.com>
2020-10-02 07:45:26 +0200

    version-1

--- src/app/explode/explode.component.ts
+++ src/app/explode/explode.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, ComponentFactoryResolver, HostListener, OnInit} from '@angular/core';
 import html2canvas from 'html2canvas';
 
 @Component({
@@ -6,89 +6,100 @@ import html2canvas from 'html2canvas';
   template: '<ng-content></ng-content>',
 })
 export class ExplodeComponent implements OnInit {
+  targetting = false;
   constructor() {}
 
   ngOnInit(): void {}
 
   @HostListener('contextmenu', ['$event'])
   async explodeIt(ev: MouseEvent) {
+    /** BOOM! get the target out of the event */
     const target = ev.target as HTMLElement;
-    console.log(target);
     ev.preventDefault();
-    const canvas = await html2canvas(target);
-    const ctx = canvas.getContext('2d');
-    
-    const {particleCtx, particleCanvas} = createParticleCanvas();
-    
-    const reductionFactor = 17;
-    
-    // Get the color data for our button
-    const width = target.offsetWidth;
-    const height = target.offsetHeight;
-    const colorData = ctx.getImageData(0, 0, width, height).data;
-    const particles: ExplodingParticle[] = [];
-
-    // Keep track of how many times we've iterated (in order to reduce
-    // the total number of particles create)
-    let count = 0;
-
-    // Go through every location of our button and create a particle
-    for (let localX = 0; localX < width; localX++) {
-      for (let localY = 0; localY < height; localY++) {
-        if (count % reductionFactor === 0) {
-          const index = (localY * width + localX) * 4;
-          const rgbaColorArr = colorData.slice(index, index + 4);
-
-          const bcr = target.getBoundingClientRect();
-          const globalX = bcr.left + localX;
-          const globalY = bcr.top + localY;
-          console.log(globalX,globalY)
-
-          particles.push(new ExplodingParticle(globalX, globalY, rgbaColorArr));
-        }
-        count++;
-      }
-    }
-    /** "play" the explosion */
-    await updateExplosion(particles, particleCtx);
-    document.body.removeChild(particleCanvas)
+    makeItGoBoom(target);
   }
 }
 
-/* An "exploding" particle effect that uses circles */
-class ExplodingParticle {
-  // Set how long we want our particle to animate for
-  animationDuration = 1000; // in ms
-  startTime = Date.now();
+/**
+ * The code behind this point is using pure DOM manipulation, and can be used inside any framework
+ * Its based loosely on the blogpost referenced at the bottom.
+ */
 
+async function makeItGoBoom(target: HTMLElement) {
+  const inputCanvas = await html2canvas(target);
+  const sourceCtx = inputCanvas.getContext('2d');
+
+  const {particleCtx, particleCanvas} = createParticleCanvas();
+
+  // Get the color data for our button
+  const width = target.offsetWidth;
+  const height = target.offsetHeight;
+  const colorData = sourceCtx.getImageData(0, 0, width, height).data;
+  const particles: ExplodingParticle[] = [];
+
+  const sampleRate = Math.max(Math.floor(10 + Math.random() * 10), Math.floor(width * height * 0.0001));
+
+  let count = 0;
+
+  for (let localX = 0; localX < width; localX++) {
+    for (let localY = 0; localY < height; localY++) {
+      if (count % sampleRate === 0) {
+        const index = (localY * width + localX) * 4;
+        const rgbaColorArr = colorData.slice(index, index + 4);
+        if (rgbaColorArr.every((color) => color === 255)) {
+          // skip full white pixels. and don't count them for the samplerate!
+          count--;
+          continue;
+        }
+
+        const bcr = target.getBoundingClientRect();
+        const globalX = bcr.left + localX;
+        const globalY = bcr.top + localY;
+
+        particles.push(new ExplodingParticle(globalX, globalY, rgbaColorArr, particleCtx));
+      }
+      count++;
+    }
+  }
+  /** "play" the explosion */
+  await triggerExplosion(particles);
+  document.body.removeChild(particleCanvas);
+}
+
+class ExplodingParticle {
   // Set the speed for our particle
   speed = {
-    x: -5 + Math.random() * 10,
-    y: -5 + Math.random() * 10,
+    x: -10 + Math.random() * 20,
+    y: -10 + Math.random() * 20,
   };
 
   // Size our particle
-  radius = 5 + Math.random() * 5;
+  radius = 10 + Math.random() * 5;
+  // set the reduction factor
+  reduce = (10 + Math.random() * 25) / 100;
 
-  // Set a max time to live for our particle
-  life = 30 + Math.random() * 10;
-  remainingLife = this.life;
+  constructor(
+    private startX: number,
+    private startY: number,
+    private rgbArray: Uint8ClampedArray,
+    public ctx: CanvasRenderingContext2D
+  ) {}
 
-  constructor(private startX: number, private startY: number, private rgbArray: Uint8ClampedArray) {}
+  get done() {
+    // calculate "done"
+    return this.radius <= 3;
+  }
 
-  // This function will be called by our animation logic later on
-  draw(ctx) {
-
-    if (this.remainingLife > 0 && this.radius > 0) {
+  draw() {
+    if (this.radius > 0) {
       // Draw a circle at the current location
-      ctx.beginPath();
-      ctx.arc(this.startX, this.startY, this.radius, 0, Math.PI * 2);
-      ctx.fillStyle = 'rgba(' + this.rgbArray[0] + ',' + this.rgbArray[1] + ',' + this.rgbArray[2] + ', 1)';
-      ctx.fill();
+      this.ctx.beginPath();
+      this.ctx.arc(this.startX, this.startY, this.radius, 0, Math.PI * 2);
+      this.ctx.fillStyle = `rgba(${this.rgbArray[0]}, ${this.rgbArray[1]}, ${this.rgbArray[2]} , 1)`;
+      this.ctx.fill();
 
       // Update the particle's location and life
-      this.remainingLife--;
-      this.radius -= 0.25;
+      this.radius -= this.reduce;
       this.startX += this.speed.x;
       this.startY += this.speed.y;
     }
@@ -121,29 +132,26 @@ function createParticleCanvas() {
   return {particleCtx, particleCanvas};
 }
 
-function updateExplosion(particles, particleCtx) {
+function triggerExplosion(particles: ExplodingParticle[]) {
+  const clearOldParticles = (p) => p.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
   return new Promise((resolve) => {
-    // Clear out the old particles
-    if (typeof particleCtx !== 'undefined') {
-      particleCtx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    }
-    const update = () => {
-      // Draw all of our particles in their new location
-      for (let i = 0; i < particles.length; i++) {
-        particles[i].draw(particleCtx);
-
-        // Simple way to clean up if the last particle is done animating
-        if (i === particles.length - 1) {
-          let percent = (Date.now() - particles[i].startTime) / particles[i].animationDuration;
-
-          if (percent > 1) {
-            return resolve();
-          }
-        }
+    const explosionStep = (list: ExplodingParticle[]) => {
+      list[0] && clearOldParticles(list[0]);
+      for (const particle of list) {
+        particle.draw();
       }
-      // Animate performantly
-      window.requestAnimationFrame(update);
+
+      if (list.length === 0) {
+        return resolve();
+      }
+      // iterate over particles that aren't done.
+      window.requestAnimationFrame(() => explosionStep(list.filter((p) => !p.done)));
     };
-    update();
+
+    explosionStep(particles);
   });
 }
+
+/**
+ * The idea of the particle effect can be found in this article: https://css-tricks.com/adding-particle-effects-to-dom-elements-with-canvas/
+ */
