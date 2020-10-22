b9e4c4ea71c7a72e1b6e1ca43e6ba5a28bbe76f3
Kate Sky <katesky@MacBook-Pro.local>
2020-10-21 21:02:37 -0500

    format

--- src/app/common/busy-http-interceptor.ts
+++ src/app/common/busy-http-interceptor.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { finalize, delay } from 'rxjs/operators';  
-import { BusyService } from './busy.service';
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { finalize, delay } from "rxjs/operators";
+import { BusyService } from "./busy.service";
 
 @Injectable()
 export class BusyHttpInterceptor implements HttpInterceptor {
+  constructor(private readonly busyService: BusyService) {}
 
-  constructor(private readonly busyService: BusyService) { }
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const message = request.method === 'GET' ? 'Loading...' : 'Saving...';
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const message = request.method === "GET" ? "Loading..." : "Saving...";
     this.busyService.increment(message);
 
     return next.handle(request).pipe(
