028138692b1273c4311601800c4b4f0b283e21e2
Shai Reznik <shairez@users.noreply.github.com>
2020-10-22 15:48:58 +0300

    first step

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,1 +1,55 @@
+import { HeroesComponent } from './heroes.component';
 
+import { TestBed } from '@angular/core/testing';
+import { Spy, createSpyFromClass } from 'jasmine-auto-spies';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+describe('HeroesComponent', () => {
+  let componentUnderTest: HeroesComponent;
+  let heroServiceSpy: Spy<HeroService>;
+
+  let actualResult: any;
+
+  Given(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        HeroesComponent,
+        {provide: HeroService, useValue: createSpyFromClass(HeroService)}
+      ]
+    });
+
+    componentUnderTest = TestBed.inject(HeroesComponent);
+    heroServiceSpy = TestBed.inject<any>(HeroService);
+
+    actualResult = undefined;
+
+  });
+
+  describe('METHOD: getHeroes', () => {
+
+    let fakeHeroes: Hero[];
+
+    Given(() => {
+      fakeHeroes = [
+        {
+          id: 11111111,
+          name: 'Bonnie'
+        }
+      ];
+
+      heroServiceSpy.getHeroes.and.nextWith(fakeHeroes);
+    });
+
+    When(() => {
+      componentUnderTest.getHeroes();
+    });
+
+    Then('saving the heroes locally', () => {
+      expect(componentUnderTest.heroes).toEqual(fakeHeroes);
+    });
+    
+    
+
+  });
+});
