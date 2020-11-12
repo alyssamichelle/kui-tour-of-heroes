bed266d8ebe9d79a121ccf494ddce07d764bf1d1
Alyssa Nicoll <alyssaamnell@gmail.com>
2020-11-12 16:14:31 -0600

    update to v11

--- src/app/hero-hire/hero-hire.component.spec.ts
+++ src/app/hero-hire/hero-hire.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeroHireComponent } from './hero-hire.component';
 
@@ -6,7 +6,7 @@ describe('HeroHireComponent', () => {
   let component: HeroHireComponent;
   let fixture: ComponentFixture<HeroHireComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroHireComponent ]
     })
