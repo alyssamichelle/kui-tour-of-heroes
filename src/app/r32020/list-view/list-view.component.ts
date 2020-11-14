d13cb551750ccaf01bf0e6cf8c33b92422c71bf7
Alyssa Nicoll <alyssaamnell@gmail.com>
2020-11-14 16:54:59 -0600

    adjust styles for multiple list-views in one row

--- src/app/r32020/list-view/list-view.component.ts
+++ src/app/r32020/list-view/list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ponies } from '../../ponies';
 
 import { Hero } from '../../hero';
